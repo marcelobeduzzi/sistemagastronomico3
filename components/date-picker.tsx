751e8bc9f180514f8bd47cd8c1322be31e69e692
marcelobeduzzi <mbeduzzi@gmail.com>
2025-05-15 09:28:08 -0300

    Se crearon archivos exportadores en las rutas de date picker viejas

---
+++ components/date-picker.tsx
@@ -0,0 +1,6 @@
+"use client"
+
+// Este archivo es para mantener compatibilidad con el código existente
+// Reexporta el DatePicker unificado desde components/ui/date-picker.tsx
+export { DatePicker } from "@/components/ui/date-picker"
+export default { DatePicker }
